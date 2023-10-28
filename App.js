db53dd28012dea97be5a671f0e59497e1fffb1b5
Aman <pawaraman290@gmail.com>
2023-10-28 23:56:29 +0530

    Episode-03: Laying the foundation

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is Namaste React"),
-    React.createElement("h2", {}, "By Akashy Saini"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-]);
 
-//JSX
+// JSX - HTML-Like or XML-Like syntax
 
-console.log(parent); // object
+// JSX (transpiled before it reaches the JS) - PARCEL - Babel
+
+// JSX => Babel transpiles it to React.createElement => ReactElement.JS Object => HTMLElement(render);
+
+// React Functional Component
+
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX 🚀
+  </h1>
+);
+
+const number = 100;
+
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <Title></Title>
+    {Title()}
+    {number}
+    <h1>Namaste React Functional Component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
